65394bd35db8913ed51e118dabdd6b2bfca3b3d2
Mac Pro <macpro@localhost.(none)>
2012-06-06 11:28:59 +0800

    Initial Commit

---
+++ FlipViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlipViewTest
+//
+//  Created by Mac Pro on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FlipViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlipViewTest
+//
+//  Created by Mac Pro on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
